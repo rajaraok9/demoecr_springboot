1a4d73364a99208f1c40f2ff2d3749579bdb2723
rajarao.kaveliga <rajaraok9@gmail.com>
2024-09-02 22:09:47 +0530

    COmmit

---
+++ src/main/java/org/modmed/demoecr_springboot/DemoecrSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.modmed.demoecr_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoecrSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoecrSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/modmed/demoecr_springboot/DemoecrSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.modmed.demoecr_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoecrSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
